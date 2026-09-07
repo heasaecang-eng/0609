ee5ae90fa9a41bffbf3dc972a2485582228356d1
heasaecang-eng <heasaecang@gmail.com>
2026-09-07 15:40:05 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("06/09/2552")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("06/09/2009")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("HAPPY BIRTHDAY!")
+})
